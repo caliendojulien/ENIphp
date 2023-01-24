fbb5d76a652e06cfba5fbfc3bc713b96e95b8edb
jcaliendo <caliendo@hotmail.fr>
2023-01-24 15:30:52 +0100

    POO

--- index.php
+++ index.php
@@ -21,24 +21,24 @@
     <li><a href="1-bases/fonctionsUtiles/index.php">Les fonctions utiles</a></li>
 <!--    <li><a href="1-bases/tp3/index.php">TP 3 : fonctions utiles</a></li>-->
     <li><a href="1-bases/fonctions/index.php">Les fonctions</a></li>
-<!--    <li><a href="1-bases/tp4/index.php">TP 3 : fonctions</a></li>-->
+    <li><a href="1-bases/tp4/index.php">TP 3 : fonctions</a></li>
 <!--    <li><a href="1-bases/expressionsRationnelles/index.php">Les expressions rationnelles</a></li>-->
 <!--    <li><a href="1-bases/tp5/index.php">TP 5 : expressions rationnelles</a></li>-->
 </ol>
-<!--<h2>La programmation orientée objet</h2>-->
-<!--<ol>-->
-<!--    <li><a href="2-poo/creationEtUtilisationDUneClasse/index.php">la création et utilisation d'une classe</a></li>-->
-<!--    <li><a href="2-poo/heritage/index.php">L'héritage</a></li>-->
-<!--    <li><a href="2-poo/static/index.php">Les attributs et méthodes de classe</a></li>-->
-<!--    <li><a href="2-poo/classeAbstraite/index.php">Les classes abstraites</a></li>-->
-<!--    <li><a href="2-poo/tp1/index.php">TP 1 : classes</a></li>-->
-<!--    <li><a href="2-poo/interface/index.php">Les interfaces</a></li>-->
-<!--    <li><a href="2-poo/exceptions/index.php">Les exceptions</a></li>-->
+<h2>La programmation orientée objet</h2>
+<ol>
+    <li><a href="2-poo/creationEtUtilisationDUneClasse/index.php">la création et utilisation d'une classe</a></li>
+    <li><a href="2-poo/heritage/index.php">L'héritage</a></li>
+    <li><a href="2-poo/static/index.php">Les attributs et méthodes de classe</a></li>
+    <li><a href="2-poo/classeAbstraite/index.php">Les classes abstraites</a></li>
+    <li><a href="2-poo/tp1/index.php">TP 1 : classes</a></li>
+    <li><a href="2-poo/interface/index.php">Les interfaces</a></li>
+    <li><a href="2-poo/exceptions/index.php">Les exceptions</a></li>
 <!--    <li><a href="2-poo/namespace/index.php">Les espaces de nom (namespace)</a></li>-->
 <!--    <li><a href="2-poo/serialisation/index.php">La sérialisation</a></li>-->
-<!--    <li><a href="2-poo/autoLoad/index.php">Le chargement automatique des classes</a></li>-->
+    <li><a href="2-poo/autoLoad/index.php">Le chargement automatique des classes</a></li>
 <!--    <li><a href="2-poo/clone/index.php">Le clonage d'une classe</a></li>-->
-<!--</ol>-->
+</ol>
 <!--<h2>Les formulaires</h2>-->
 <!--<ol>-->
 <!--    <li><a href="3-formulaires/traitementAPart/index.php">Les Formulaires avec traitement à part</a></li>-->
