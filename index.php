9979fab51124b6d86ab2d6a5f3b59c686c46664e
Caliendo Julien <caliendo@hotmail.fr>
2023-01-27 08:31:28 +0100

    Mini ménage

--- index.php
+++ index.php
@@ -19,11 +19,8 @@
     <li><a href="1-bases/include_require/index.php">L'inclusion de fichiers</a></li>
     <li><a href="1-bases/tp2/index.php">TP 2 : tableaux</a></li>
     <li><a href="1-bases/fonctionsUtiles/index.php">Les fonctions utiles</a></li>
-<!--    <li><a href="1-bases/tp3/index.php">TP 3 : fonctions utiles</a></li>-->
     <li><a href="1-bases/fonctions/index.php">Les fonctions</a></li>
     <li><a href="1-bases/tp4/index.php">TP 3 : fonctions</a></li>
-<!--    <li><a href="1-bases/expressionsRationnelles/index.php">Les expressions rationnelles</a></li>-->
-<!--    <li><a href="1-bases/tp5/index.php">TP 5 : expressions rationnelles</a></li>-->
 </ol>
 <h2>La programmation orientée objet</h2>
 <ol>
@@ -34,10 +31,7 @@
     <li><a href="2-poo/tp1/index.php">TP 1 : classes</a></li>
     <li><a href="2-poo/interface/index.php">Les interfaces</a></li>
     <li><a href="2-poo/exceptions/index.php">Les exceptions</a></li>
-<!--    <li><a href="2-poo/namespace/index.php">Les espaces de nom (namespace)</a></li>-->
-<!--    <li><a href="2-poo/serialisation/index.php">La sérialisation</a></li>-->
     <li><a href="2-poo/autoLoad/index.php">Le chargement automatique des classes</a></li>
-<!--    <li><a href="2-poo/clone/index.php">Le clonage d'une classe</a></li>-->
 </ol>
 <h2>Les formulaires</h2>
 <ol>
@@ -52,8 +46,6 @@
     <li><a href="4-accesDonnees/connexion/index.php">La connexion</a></li>
     <li><a href="4-accesDonnees/requeteNonPreparee/index.php">Les requêtes non préparées</a></li>
     <li><a href="4-accesDonnees/requetePreparee/index.php">Les requêtes préparées</a></li>
-<!--    <li><a href="4-accesDonnees/proceduresStockees/index.php">Les procédures stockées</a></li>-->
-<!--    <li><a href="4-accesDonnees/transactions/index.php">Les transactions</a></li>-->
     <li><a href="4-accesDonnees/tp1/index.php">TP 1 : Requêter une base de données</a></li>
 </ol>
 <h2>Les échanges de données entre le client et le serveur</h2>
@@ -61,7 +53,6 @@
     <li><a href="5-clientserveur/redirections/index.php">Redirections</a></li>
     <li><a href="5-clientserveur/cookies/index.php">Cookies</a></li>
     <li><a href="5-clientserveur/sessions/index.php">Sessions</a></li>
-<!--    <li><a href="5-clientserveur/ajax/index.php">AJAX</a></li>-->
     <li><a href="5-clientserveur/tp1/index.php">TP 1 : Échanges client-serveur</a></li>
 </ol>
 <h2>Corrections non officielles</h2>
